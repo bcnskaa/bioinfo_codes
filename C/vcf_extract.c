2d42f1028f92d6ed818b5072c9ec3614988f4299
bcnskaa <bcnskaa@gmail.com>
2014-06-17 06:32:54 +0800

    Update vcf_extract.c

--- C/vcf_extract.c
+++ C/vcf_extract.c
@@ -24,10 +24,10 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-/*
+ *
+ *
+ *
+ *
  * vcf_extract.c
  *
  *  vcf_extract: a small tool for extracting data from a data in VCF format
