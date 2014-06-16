2b4e3b10785bead05f83ca8783eda728c28951c8
bcnskaa <bcnskaa@gmail.com>
2014-06-17 05:51:16 +0800

    Create filter_dbsnp.py

---
+++ filter_dbsnp.py
@@ -0,0 +1,58 @@
+#########################
+#
+# Copyright (c) 2013, SK Woolf <bcnskaa@gmail.com>.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+#########################
+import _thread
+import time
+import traceback
+import math
+
+
+
+
+def extract_snp_by_chrom(fn_in, fn_out, chr_id):
+    line_n = 0
+    export_n = 0
+    
+    print("Parsing ", fn_in, " for ", chr_id, " and exporting to ", fn_out)
+    
+    with open(fn_in, "r") as f_in, open(fn_out, "w") as f_out:
+        for line in f_in:
+            if line_n == 0:
+                f_out.write(line)
+            else:
+                items = line.split("\t")
+                if items[2] == chr_id:
+                    f_out.write(line)
+                    export_n += 1
+            line_n += 1
+    print("Number of SNP exported: ", export_n)
+    
+    retu
